4edc01044163b9b1c6e2a26b58d0de17be87b44f
Marco <mkroemer384@gmail.com>
2025-02-27 23:29:28 +0100

    lecture 2.1

--- start/lecture2_1/app.js
+++ start/lecture2_1/app.js
@@ -6,8 +6,9 @@ class App{
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
     
-		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth/window.innerHeight, 0.1, 100);
-		this.camera.getWorldPosition.set( 0,0,4);
+		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth/
+			window.innerHeight, 0.1, 100);
+		this.camera.position.setSize( 0,0,4);
 		this.scene = new THREE.Scene();
 		this.scene.background = new THREE.Color ( 0xaaaaaa );
 		
